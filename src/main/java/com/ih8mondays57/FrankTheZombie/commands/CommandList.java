6c8992b7f661d83725f8f9a56246722ed8bb23e5
Ben Monday <bmonday@unomaha.edu>
2024-08-29 17:09:41 -0500

    Initial Commit

--- src/main/java/com/ih8mondays57/FrankTheZombie/commands/CommandList.java
+++ src/main/java/com/ih8mondays57/FrankTheZombie/commands/CommandList.java
@@ -3,8 +3,16 @@ package com.ih8mondays57.FrankTheZombie.commands;
 public class CommandList {
 
     public String returnCommandListMessage() {
-        return "```List of commands:\nz!help - Displays basic info about the bot.\nz!commandlist - Displays all commands (what you are seeing now).\nz!bugsnax - Gives the user a random Bugsnack image." +
-                "\nz!trap - Gives the user a random trap image from the history of zombies.\nz!sholmes - Gives the user a random Sholmes image." +
-                "\nz!play <link> - Plays audio from a link in the voice channel.\nz!stop - Stops playing the current audio track.```";
+        return """
+                ```List of commands:
+                z!help - Displays basic info about the bot.
+                z!commandlist - Displays all commands (what you are seeing now).
+                z!bugsnax - Gives the user a random Bugsnack image.\
+                
+                z!trap - Gives the user a random trap image from the history of zombies.
+                z!sholmes - Gives the user a random Sholmes image.\
+                
+                z!play <link> - Plays audio from a link in the voice channel.
+                z!stop - Stops playing the current audio track.```""";
     }
 }
